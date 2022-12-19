7a1cb630adeaf415be704e0f325edfebd2ff0a5f
JaeHyeonYoo <dbwogus308@naver.com>
2022-12-19 16:37:54 +0900

    스프링 적용해보기

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,21 +8,27 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration       //spring으로 등록하기 위함
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public MemberRepository memberRepository(){  //객체를 배꿀 때 코드 변경이 효율적이다.
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
-        //return new FixDiscountPolicy();  //구현 객체 변경하기
+        //return new FixDiscountPolicy();  //구현 객체 변경하채
         return new RateDiscountPolicy();   //구현체를 변경할 때는 AppConfig 만 수정하면된다.
     }
 }
