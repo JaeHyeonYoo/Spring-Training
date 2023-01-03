d3d7a9fcbdd40c6f173bde24e0f88bcf8da714dc
JaeHyeonYoo <dbwogus308@naver.com>
2023-01-03 20:30:24 +0900

    컴포넌트 스캔 적용해보기

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(  //수동으로 설정등록한 AppConfig를 제외
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
